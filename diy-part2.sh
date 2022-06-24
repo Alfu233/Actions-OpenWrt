f0f2d7b4f47bf534c7b8ae68ada875ab2c6e1aa4
Alfu233 <71091764+Alfu233@users.noreply.github.com>
2022-06-25 01:16:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#1. Replace with JerryKuKu’s Argon
+rm openwrt/package/lean/luci-theme-argon -rf
